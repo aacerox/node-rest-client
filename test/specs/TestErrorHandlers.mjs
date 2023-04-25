0f1e8e30e7489dfdde25e74e80f82654a51473ca
aacerox <alex.acero@gmail.com>
2023-04-25 23:18:12 +0200

    refactor: global nrc refactor to modern JS
    - chore: add code comments
    - refactor:  backup refactor
    - refactor: pass all tests
    - refactor: finished client refactor
    - feat: add promise execution
    - refactor: add debug function
    - test: refactor test and new post promised tests
    - refactor: rename main modules
    - refactor: configure build for module and cjs
    - refactor: delete mistaken files
    - refactor: ignore files
    - refactor: deleted un-required files
    - refactor: add cjs y module test projects
    - chore: ignore files
    - refactor: align test to module build
    - refactor: improve code

---
+++ test/specs/TestErrorHandlers.mjs
@@ -0,0 +1,45 @@
+import server from '../server/mock-server.mjs';
+import Client from '../../dist/mjs/NodeRestClient.js';
+
+
+describe('Error Handlers', function() {
+  // eslint-disable-next-line no-invalid-this
+  this.timeout(150000);
+
+  before(function() {
+    server.listen(4444);
+    console.log('server started on port 4444');
+  });
+
+  describe('Client Error Handlers', function() {
+    it('handle error with client handler', function(done) {
+      const client = new Client();
+      client.on('error', function(err) {
+        done();
+      });
+      client.get(server.baseURL + '/json/error', function(data, response) {
+        client.emit('error', response.status);
+      });
+    });
+  });
+
+  describe('#Request Error Handlers', function() {
+    it('handle error with request handler', function(done) {
+      const client = new Client();
+
+      const req =client.get(server.baseURL + '/json/error',
+          function(data, response) {
+            req.emit('error', response.status);
+          });
+
+      req.on('error', function(err) {
+        done();
+      });
+    });
+  });
+
+  after(function() {
+    server.close();
+    console.log('server stopped');
+  });
+});
