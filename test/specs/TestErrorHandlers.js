98b906368f0dddf848dac1d28e196e9809072dbc
aacerox <alex.acero@gmail.com>
2023-05-18 15:51:16 +0200

    refactor: finished client refactor

--- test/specs/TestErrorHandlers.js
+++ test/specs/TestErrorHandlers.js
@@ -1,55 +1,45 @@
-import server from "../server/mock-server.js"
-import Client from '../../lib/node-rest-client.js'
+import server from '../server/mock-server.js';
+import Client from '../../lib/node-rest-client.js';
 
 
-describe('Error Handlers', function () {
-	
-  this.timeout(150000)
-	
-  before(function () {
-    server.listen(4444)
-    console.log("server started on port 4444")
-  })
+describe('Error Handlers', function() {
+  // eslint-disable-next-line no-invalid-this
+  this.timeout(150000);
 
-  describe("Client Error Handlers",function(){
+  before(function() {
+    server.listen(4444);
+    console.log('server started on port 4444');
+  });
 
+  describe('Client Error Handlers', function() {
+    it('handle error with client handler', function(done) {
+      const client = new Client();
+      client.on('error', function(err) {
+        done();
+      });
+      client.get(server.baseURL + '/json/error', function(data, response) {
+        client.emit('error', response.status);
+      });
+    });
+  });
 
-    it("handle error with client handler", function(done){
-      let client = new Client()
-      client.on('error', function(err){        
-        done()
-      })
-      client.get(server.baseURL + "/json/error", function(data, response){
-        client.emit('error', response.status)  
-      })
+  describe('#Request Error Handlers', function() {
+    it('handle error with request handler', function(done) {
+      const client = new Client();
 
-    })
+      const req =client.get(server.baseURL + '/json/error',
+          function(data, response) {
+            req.emit('error', response.status);
+          });
 
+      req.on('error', function(err) {
+        done();
+      });
+    });
+  });
 
-
-
-  })
-
-describe("#Request Error Handlers",function(){
-
-   it("handle error with request handler", function(done){
-      let client = new Client()
-
-      let req =client.get(server.baseURL + "/json/error", function(data, response){
-        req.emit('error', response.status) 
-      })
-
-      req.on('error',function(err){
-        done()
-      })
-
-    })
-
-})
-
-after(function () {
-  server.close()
-  console.log("server stopped")
-})
-
-})
+  after(function() {
+    server.close();
+    console.log('server stopped');
+  });
+});
