7c53c787c76c17cadbebfc587ba33316d4f6d461
aacerox <alex.acero@gmail.com>
2023-06-09 10:07:03 +0200

    refactor: deleted un-required files

--- test/test-node-module.js
+++
@@ -1,5 +0,0 @@
-import Client from '../lib/NodeRestClient.js';
-
-const client = new Client();
-
-console.log('client es ', client);
--- test/test-node-script.cjs
+++
@@ -1,5 +0,0 @@
-const Client = require('../lib/NodeRestClient.js');
-
-const client = new Client();
-
-console.log('client es ', client);
